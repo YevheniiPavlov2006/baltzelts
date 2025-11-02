41c134cd5f5752693a1f6ce88cf68b6f77d17f8c
YevheniiPavlov2006 <jackpavlov9@gmail.com>
2025-11-02 09:17:18 +0200

    merge

--- phonenumber.js
+++ phonenumber.js
@@ -34,6 +34,49 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+const select = document.querySelector('.phone-input-select');
+const selected = select.querySelector('.selected');
+const options = select.querySelectorAll('.options li');
+const hiddenInput = document.querySelector('#phone_code');
+
+options.forEach(option => {
+  option.addEventListener('click', () => {
+    const code = option.dataset.code;
+    selected.innerHTML = option.innerHTML; // Обновляем выбранный элемент
+    hiddenInput.value = code; // Устанавливаем значение в hidden input
+
+    // Триггерим событие change, чтобы можно было отловить изменение
+    const event = new Event('change', { bubbles: true });
+    hiddenInput.dispatchEvent(event);
+
+    // (по желанию) закрываем список, если у тебя есть логика для этого
+    select.classList.remove('open');
+  });
+});
+
+
+document.querySelector('#phone_code').addEventListener('change', (e) => {
+  console.log('Изменён телефонный код:', e.target.value);
+});
+
+
+
+/*--------------------------только цифры----------------------*/
+
+const phoneInput = document.querySelector('.phone-input');
+
+phoneInput.addEventListener('input', (e) => {
+  // Удаляем всё, что не цифра
+  e.target.value = e.target.value.replace(/\D/g, '');
+
+  // Ограничиваем длину (на случай, если вставили текст)
+  if (e.target.value.length > 9) {
+    e.target.value = e.target.value.slice(0, 9);
+  }
+});
+
+
+
 
  
 
